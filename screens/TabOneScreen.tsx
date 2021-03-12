eb3075f372f6e04393b0925e11f796b33e231a33
NakanoNamiki <namiki.nakano.vertex@gmail.com>
2021-03-12 23:28:14 +0900

    first commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,15 +1,24 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import * as React from "react";
+import { TouchableOpacity, Image, StyleSheet } from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import logo from "../assets/images/logo.png";
 
-export default function TabOneScreen() {
+export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.instraction}>
+        To share a photo from your phone with a friend, just press the button
+        below!
+      </Text>
+      <TouchableOpacity
+        onPress={() => alert("押してくれてありがとう")}
+        style={styles.buttonUi}
+      >
+        <Text style={styles.buttonText}>Pick a photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -17,16 +26,26 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title: {
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 100,
+  },
+  instraction: {
+    color: "#888",
+    fontSize: 28,
+    marginHorizontal: 25,
+  },
+  buttonUi: {
+    backgroundColor: "blue",
+    marginTop: 10,
+  },
+  buttonText: {
     fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    color: "white",
   },
 });
